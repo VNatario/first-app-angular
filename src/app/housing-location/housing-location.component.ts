175841c9252a80be05e947d76d29c3c66817eeea
Vitor Natario <natariovitor@gmail.com>
2023-09-10 00:25:52 -0300

    feat: add imports from housing-location component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrls: ['./housing-location.component.css']
+  template: ` <p>housing-location works!</p> `,
+  styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent {
-
-}
+export class HousingLocationComponent {}
